11fdd3475a85ba985becfd153c9983ec532977d6
Ezey <emre.zeyrek@bil.omu.edu.tr>
2013-05-07 21:02:32 +0300

    duzenleme

---
+++ java/Web_programlama_odev_7/wheel_of_fortune/src/applet.java
@@ -0,0 +1,12 @@
+
+
+import javax.swing.JApplet;
+
+public class applet extends JApplet {
+	public void init(){
+		WindowUtilities.setNativeLookAndFeel();
+		setContentPane(new textpanel());
+	}
+	
+
+}
