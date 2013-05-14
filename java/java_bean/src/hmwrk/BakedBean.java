3505549ef76aa1be749999843e4bddc94f6043cc
Ezey <emre.zeyrek@bil.omu.edu.tr>
2013-05-14 21:47:54 +0300

    duzenleme

---
+++ java/java_bean/src/hmwrk/BakedBean.java
@@ -0,0 +1,24 @@
+package hmwrk;
+
+public class BakedBean {
+	
+	private String name = "Emre";
+	private String surname = "ZEYREK";
+	
+	public String getname() {
+		return(name);
+	}
+	
+	public void setname(String newname) {
+		name = newname;
+	}
+	
+	public String getsurname() {
+		return(surname);
+	}
+	
+	public void setsurname(String n_surname) {
+		surname = n_surname;
+	}
+
+}
